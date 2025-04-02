53180eaa42fd90f6053b065dab846e1c0effcc6a
first.demo <rahulig01yadav@gmail.com>
2025-04-02 21:44:45 +0530

    adding logic

--- Rock-Paper-Scissors/app.js
+++ Rock-Paper-Scissors/app.js
@@ -1,13 +1,62 @@
 let us=0;
 let bos=0;
+const play = document.getElementById("play");
 const allc=document.querySelectorAll(".choi")
+const users=document.querySelector("#users");
+const botsc=document.querySelector("#bots");
+
+
+
+
 const gbot=()=>{
-    const option=["rock","paper","scissor"]
+    const option=["rock","paper","scissors"];
     const ri=Math.floor(Math.random()*3);
-    return ri;
+    return option[ri];
+}
+const draw=()=>{
+    console.log("game was drawn");
+    play.innerText="Game Draw!!";
+    play.style.backgroundColor="orange";
+}
+const showin=(uwin,ui,ci)=>{
+    if(uwin){
+        us++;
+        users.innerText=us;
+        console.log("user win");
+        play.innerText=`You win!! Your "${ui}" beats "${ci}".`;
+        play.style.backgroundColor="green";
+       
+    }else{
+        bos++;
+        botsc.innerText=bos;
+        console.log("bot win");
+        play.innerText=`You Lose!! "${ci}" beats "${ui}".`;
+        play.style.backgroundColor="red";
+      
+    }
+
 }
 const playg=(use)=>{
     console.log("choice was ",use);
+    const ci=gbot();
+    console.log("choice of bot was :",ci);
+    if(use===ci){
+        draw();
+
+    }else{
+        let uwin=true;
+
+
+        if(use==="rock"){
+          uwin=ci==="paper"? false :true;
+        }else if(use==="scissors"){
+            uwin=ci==="rock"? false :true;
+        }
+        else if(use==="paper"){
+            uwin=ci==="scissors" ? false:true;
+        }
+        showin(uwin,use,ci);
+    }
 }
 allc.forEach((choi) => {
     console.log(choi);
